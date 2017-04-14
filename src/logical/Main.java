f3f66e9aa43356b6ed3be693fb1d0d74822640e5
felixalguzman <felixlvl@gmail.com>
2017-04-13 22:01:51 -0400

    Agregando Archivo

--- src/logical/Main.java
+++ src/logical/Main.java
@@ -1,11 +1,23 @@
 package logical;
 
+import java.io.FileNotFoundException;
+
 /**
  * Created by felix on 4/13/2017.
  */
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hola");
+    public static void main(String[] args) throws FileNotFoundException {
+            int[] ver = {7,37,59,82,99,115,133,165,188,197 };
+            Grafo g = new Grafo("Archivo/Archivo.txt");
+            int[] caminos = g.caminoCorto();
+
+        for (int i: ver){
+            System.out.print(caminos[i] + " ");
+
+        }
+
+
+
     }
 }
