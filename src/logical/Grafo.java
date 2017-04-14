13b2bc43bfb4afc7244eab3c91bbb25bb8e8fe12
felixalguzman <felixlvl@gmail.com>
2017-04-13 20:28:37 -0400

    Creando Clase Grafo

---
+++ src/logical/Grafo.java
@@ -0,0 +1,16 @@
+package logical;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/**
+ * Created by felix on 4/13/2017.
+ */
+public class Grafo {
+
+    private ArrayList<ArrayList<int[]>> vertices;
+    private HashSet<Integer> explorado;
+
+    public Grafo() {
+    }
+}
